61e3f81fc17047de7ddbe15b62ea8b44fc7c8681
Rocky Essel <rockyessel76@gmail.com>
2023-05-22 21:26:47 +0000

    add @vercel/analytics

--- frontend/src/root/main.tsx
+++ frontend/src/root/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx';
 import '../styles/index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppWriteContextProvider } from '../context/app-write.tsx';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
